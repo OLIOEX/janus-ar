6a2f58abde581e2b84ea15f8d9b1217078fa018c
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2026-09-15 11:02:44 +0100

    Remove frozen string literal comment

--- lib/janus-ar/active_record/connection_adapters/janus_postgresql_adapter.rb
+++ lib/janus-ar/active_record/connection_adapters/janus_postgresql_adapter.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'active_record/connection_adapters/abstract_adapter'
 require 'active_record/connection_adapters/postgresql_adapter'
 require_relative '../../../janus-ar'
